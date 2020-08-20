b31a70049610e5e5c0f6bac4a17ee959284507eb
Hugo Benício <hbobenicio@gmail.com>
2020-08-20 17:16:05 -0300

    changing default mode to interactive

--- cert-get-cli/src/app.rs
+++ cert-get-cli/src/app.rs
@@ -2,7 +2,7 @@ use log::debug;
 use cert_get_core as core;
 
 const ARG_HELP: &'static str = "HELP";
-const ARG_INTERACTIVE: &'static str = "INTERACTIVE";
+const ARG_BATCH: &'static str = "BATCH";
 const ARG_HOST: &'static str = "HOST";
 const ARG_PORT: &'static str = "PORT";
 const ARG_OUTPUT_DIR: &'static str = "OUTPUT_DIR";
@@ -17,46 +17,10 @@ struct CLIContext {
 pub fn run() -> Result<(), String> {
     let arg_matches = clap_app_new().get_matches();
 
-    let cli_context = if arg_matches.is_present(ARG_INTERACTIVE) {
-        debug!("interactive mode");
-
-        let host: String = dialoguer::Input::new()
-            .with_prompt("Server host/ip")
-            .interact()
-            .map_err(cert_get_core::map_io_err)?;
-
-        let port: String = dialoguer::Input::new()
-            .with_prompt("Server port")
-            .default(String::from("443"))
-            .interact()
-            .map_err(cert_get_core::map_io_err)?;
-
-        let output_dir: String = dialoguer::Input::new()
-            .with_prompt("Output directory")
-            .default(String::from("."))
-            .interact()
-            .map_err(cert_get_core::map_io_err)?;
-        let output_dir = std::path::Path::new(&output_dir).to_owned();
-
-        let addr = format!("{}:{}", host, port);
-
-        CLIContext { host, port, addr, output_dir }
-
+    let cli_context = if arg_matches.is_present(ARG_BATCH) {
+        parse_cli_args(&arg_matches)?
     } else {
-        debug!("non-interactive mode");
-
-        // Unwraps são seguros, pois:
-        // - host é obrigatório
-        // - port não é obrigatório, mas possui valor default
-        // - output_dir não é obrigatório, mas possui valor default
-        let host = arg_matches.value_of(ARG_HOST).unwrap().to_string();
-        let port = arg_matches.value_of(ARG_PORT).unwrap().to_string();
-        let output_dir = arg_matches.value_of(ARG_OUTPUT_DIR).unwrap().to_string();
-        let output_dir = std::path::Path::new(&output_dir).to_owned();
-
-        let addr = format!("{}:{}", host, port);
-
-        CLIContext { host, port, addr, output_dir }
+        run_interactive_mode(&arg_matches)?
     };
 
     let spinner = indicatif::ProgressBar::new_spinner();
@@ -76,6 +40,53 @@ pub fn run() -> Result<(), String> {
     Ok(())
 }
 
+fn run_interactive_mode(arg_matches: &clap::ArgMatches) -> Result<CLIContext, String> {
+    debug!("entering interactive mode");
+
+    let default_host = arg_matches.value_of(ARG_HOST).unwrap_or("localhost").to_string();
+    let host: String = dialoguer::Input::new()
+        .with_prompt("Server host/ip")
+        .default(default_host)
+        .interact()
+        .map_err(cert_get_core::map_io_err)?;
+
+    let default_port = arg_matches.value_of(ARG_PORT).unwrap_or("443").to_string();
+    let port: String = dialoguer::Input::new()
+        .with_prompt("Server port")
+        .default(default_port)
+        .interact()
+        .map_err(cert_get_core::map_io_err)?;
+
+    let default_output_dir = arg_matches.value_of(ARG_OUTPUT_DIR).unwrap_or(".").to_string();
+    let output_dir: String = dialoguer::Input::new()
+        .with_prompt("Output directory")
+        .default(default_output_dir)
+        .interact()
+        .map_err(cert_get_core::map_io_err)?;
+    let output_dir = std::path::Path::new(&output_dir).to_owned();
+
+    let addr = format!("{}:{}", host, port);
+
+    Ok(CLIContext { host, port, addr, output_dir })
+}
+
+fn parse_cli_args(arg_matches: &clap::ArgMatches) -> Result<CLIContext, String> {
+    debug!("entering batch mode (non-interactive)");
+
+    // Unwraps são seguros, pois:
+    // - host é obrigatório
+    // - port não é obrigatório, mas possui valor default
+    // - output_dir não é obrigatório, mas possui valor default
+    let host = arg_matches.value_of(ARG_HOST).unwrap().to_string();
+    let port = arg_matches.value_of(ARG_PORT).unwrap().to_string();
+    let output_dir = arg_matches.value_of(ARG_OUTPUT_DIR).unwrap().to_string();
+    let output_dir = std::path::Path::new(&output_dir).to_owned();
+
+    let addr = format!("{}:{}", host, port);
+
+    Ok(CLIContext { host, port, addr, output_dir })
+}
+
 fn clap_app_new<'a>() -> clap::App<'a, 'a> {
     clap::App::new("cert-get")
         .version("0.1.0")
@@ -87,10 +98,10 @@ fn clap_app_new<'a>() -> clap::App<'a, 'a> {
             .required(false)
             .takes_value(false)
         )
-        .arg(clap::Arg::with_name(ARG_INTERACTIVE)
-            .help("enter interactive mode")
-            .short("i")
-            .long("interactive")
+        .arg(clap::Arg::with_name(ARG_BATCH)
+            .help("enter batch mode (non-interactive)")
+            .short("b")
+            .long("batch")
             .required(false)
             .takes_value(false)
         )
@@ -98,7 +109,7 @@ fn clap_app_new<'a>() -> clap::App<'a, 'a> {
             .help("Servers host/ip")
             .short("h")
             .long("host")
-            .required_unless(ARG_INTERACTIVE)
+            .required_unless(ARG_BATCH)
             .value_name(ARG_HOST)
             .takes_value(true)
         )
